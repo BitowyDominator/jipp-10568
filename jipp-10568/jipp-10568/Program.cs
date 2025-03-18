73dc6aa064c691606a93cf18aa0c08e661ec63f2
Bitowy Dominator <w.oworuszko@o2.pl>
2025-03-18 21:09:12 +0100

    init

---
+++ jipp-10568/jipp-10568/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace jipp_10568
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
